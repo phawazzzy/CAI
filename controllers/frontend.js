ec69515cdca47add4aa2707a62446b3c3604fb1f
phawazzzy <phawazzzy@gmail.com>
2019-03-26 20:27:55 +0100

    homepage

---
+++ controllers/frontend.js
@@ -0,0 +1,20 @@
+exports.homepage = (req, res) => {
+    res.render("index", {})
+};
+
+exports.login = (req, res) => {
+    res.render("login", {})
+};
+
+exports.register = (req, res) => {
+    // res.send("i am register and i am working")
+    res.render("register", {})
+
+}
+exports.about = (req, res) => {
+    res.render("about", {});
+};
+
+exports.courses = (req, res) => {
+    res.render("courses", {})
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+let controller = require("../controllers/frontend");
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', controller.homepage);
+router.get('/login', controller.login);
+router.get('/register', controller.register);
+router.get('/about', controller.about);
+router.get('/courses', controller.courses);
+
+
+
 
 module.exports = router;
