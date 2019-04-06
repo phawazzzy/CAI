9dd4fc7ae3d3ac8b92328fedcc944f56343d2ff4
phawazzzy <phawazzzy@gmail.com>
2019-04-06 20:54:46 +0100

    dynamic classrom

--- controllers/frontend.js
+++ controllers/frontend.js
@@ -1,4 +1,6 @@
-var user = require('../models/user');
+var user = require('../models/user')
+let courses = require('../models/courses')
+
 
 
 
@@ -21,9 +23,33 @@ exports.about = (req, res) => {
     res.render("about", {});
 };
 
-exports.courses = (req, res) => {
-    res.render("courses", {})
+exports.courses = async(req, res, next) => {
+    let doc = ""
+    doc = await courses.find({})
+    res.render("courses", { doc })
 }
+
+exports.coursepage = async(req, res) => {
+    let idd = req.params.id;
+    let result = "";
+
+    result = await courses.findOne({ _id: idd });
+    res.render("course", { result })
+}
+
+// router.get("/lessons", async function(req, res, next){
+//     let result = ""
+//    result = await Lecture.find({})
+//     res.render("lessons", {result})
+//   })
+
+// router.get("/lessons/:id", async function(req, res, next){
+//     let idd = req.params.id;
+//     let result = "";
+
+//     result = await Lecture.findOne({link: idd});  
+//     res.render("template", {result})        
+// })
 
 
 exports.classroom = (req, res) => {
@@ -35,6 +61,7 @@ exports.topic = (req, res) => {
 }
 
 exports.contact = (req, res) => {
+
     res.render("contact", {})
 }
 
@@ -46,6 +73,31 @@ exports.summary = (req, res) => {
     res.render("summary", {})
 }
 
-exports.coursepage = (req, res) => {
-    res.render("course", {})
-}
+
+
+
+
+
+// exports.renderPage = function(req, res) {
+//     let navIndex = req.path.substr(1);
+//     if (typeof f[navIndex] === "undefined") {
+//         (async() => {
+//             const news = await allNews;
+
+//             res.render("frontend/404", { activeNav: "", navIndex, doc: news });
+//         })();
+//     } else {
+//         let thisPage = f[navIndex].data;
+//         let activeNav = f[navIndex].nav;
+//         (async() => {
+//             let pageData = Page.find({ tag: thisPage });
+
+//             const [dt, news, ptn] =
+//             await Promise.all(
+//                 [pageData, allNews, allPartners]
+//             );
+
+//             res.render("frontend/template", { content: dt[0], doc: news, title: navIndex.replace(/(-)+/gi, " ").toUpperCase(), activeNav, partners: ptn });
+//         })();
+//     }
+// };
