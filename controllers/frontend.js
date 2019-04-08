dabe52fa8af0c8fc64bb46586e65a5d141fb126b
phawazzzy <phawazzzy@gmail.com>
2019-04-08 10:54:38 +0100

    checking

--- controllers/frontend.js
+++ controllers/frontend.js
@@ -5,16 +5,16 @@ let courses = require('../models/courses')
 
 
 
-exports.homepage = async (req, res) => {
+exports.homepage = async(req, res) => {
     let free = "";
     let featured = "";
-    free = await courses.find({type: "free"});
-    featured = await courses.findOne({type: "featured"});
+    free = await courses.find({ type: "free" });
+    featured = await courses.findOne({ type: "featured" });
 
     let pagename = "home";
 
     console.log(free)
-    res.render("index", {free, featured, pagename, isLoggedIn:req.isAuthenticated()})
+    res.render("index", { free, featured, pagename, isLoggedIn: req.isAuthenticated() })
 };
 
 
@@ -31,39 +31,39 @@ exports.register = (req, res) => {
 }
 exports.about = (req, res) => {
     let pagename = "about";
-    res.render("about", {pagename, isLoggedIn:req.isAuthenticated()});
-    
+    res.render("about", { pagename, isLoggedIn: req.isAuthenticated() });
+
 };
 
 exports.courses = (req, res) => {
     let pagename = "courses";
-    res.render("courses", {pagename, isLoggedIn:req.isAuthenticated()})
+    res.render("courses", { pagename, isLoggedIn: req.isAuthenticated() })
 }
 
 
 exports.classroom = (req, res) => {
     let pagename = "classroom";
-    res.render("classroom", {pagename, isLoggedIn:req.isAuthenticated()})
+    res.render("classroom", { pagename, isLoggedIn: req.isAuthenticated() })
 }
 
 exports.topic = (req, res) => {
     let pagename = "topic";
-    res.render("CMS/topic", {pagename, isLoggedIn:req.isAuthenticated()})
+    res.render("CMS/topic", { pagename, isLoggedIn: req.isAuthenticated() })
 }
 
 exports.contact = (req, res) => {
     let pagename = "contact";
-    res.render("contact", {pagename, user: req.user , isLoggedIn:req.isAuthenticated()})
+    res.render("contact", { pagename, user: req.user, isLoggedIn: req.isAuthenticated() })
     console.log(req.isAuthenticated())
 }
 
 
 exports.summary = (req, res) => {
     let pagename = "summary";
-    res.render("summary", {pagename, isLoggedIn:req.isAuthenticated()})
+    res.render("summary", { pagename, isLoggedIn: req.isAuthenticated() })
 }
 
 exports.coursepage = (req, res) => {
     let pagename = "coursepage";
-    res.render("course", {pagename, isLoggedIn:req.isAuthenticated()})
+    res.render("course", { pagename, isLoggedIn: req.isAuthenticated() })
 }
