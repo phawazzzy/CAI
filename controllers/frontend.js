55af34749f2a14fe4beaaed5c9cfeab9e06081af
Azeez Lukman <azeezlukman95@gmail.com>
2019-04-18 06:47:04 +0100

    Dynamic Navbar successfull

--- controllers/frontend.js
+++ controllers/frontend.js
@@ -1,65 +1,187 @@
-var user = require('../models/user');
-let courses = require('../models/courses')
+var user = require("../models/user");
+let courses = require("../models/courses");
 
-exports.homepage = async(req, res) => {
-    let free = "";
-    let featured = "";
-    free = await courses.find({type: "free"});
-    featured = await courses.findOne({type: "featured"});
-    let pagename = "home";
-    const authenticated = req.isAuthenticated();
-    console.log(authenticated)
-    res.render("index", {free, featured, pagename, authenticated,})
+exports.homepage = async (req, res) => {
+  let free = "";
+  let featured = "";
+  free = await courses.find({ type: "free" });
+  featured = await courses.findOne({ type: "featured" });
+  let pagename = "home";
+  const authenticated = req.isAuthenticated();
+  console.log(authenticated);
+
+  var Username = function() {
+    if (authenticated) {
+      let username = req.user.name;
+      return username;
+    } else {
+        let username = ""
+        return username;
+    }
+    
+  };
+
+  var UserEmail = function() {
+    if (authenticated) {
+      let userEmail = req.user.email;
+      return userEmail;
+    } else {
+        let userEmail = ""
+        return userEmail;
+    }
+    
+  };
+  
+  username = Username()
+  userEmail = UserEmail()
+
+  res.render("index", { free, featured, pagename, authenticated, username, userEmail });
 };
 
-
 exports.login = (req, res) => {
-    let loginError = req.flash('loginError');
-    let passwordError = req.flash('passwordError')
-    res.render("login", { loginError, passwordError })
+  let loginError = req.flash("loginError");
+  let passwordError = req.flash("passwordError");
+  res.render("login", { loginError, passwordError });
 };
 
 exports.register = (req, res) => {
-    let existence = req.flash('userExist')
-    res.render("register", { existence })
-
-}
-exports.about = (req, res) => {
-    let pagename = "about"
-    const authenticated = req.isAuthenticated();
-
-    res.render("about", {pagename, authenticated });
-    
+  let existence = req.flash("userExist");
+  res.render("register", { existence });
 };
 
+exports.about = (req, res) => {
+  let pagename = "about";
+  const authenticated = req.isAuthenticated();
+ 
+  var Username = function() {
+    if (authenticated) {
+      let username = req.user.name;
+      return username;
+    } else {
+        let username = ""
+        return username;
+    }
+    
+  };
 
-exports.courses = async(req, res, next) => {
-    let doc = ""
-    doc = await courses.find({})
-    let pagename ="courses";
-    const authenticated = req.isAuthenticated();
-    console.log(authenticated)
-    res.render("courses", { doc, pagename,authenticated})
-}
+  var UserEmail = function() {
+    if (authenticated) {
+      let userEmail = req.user.email;
+      return userEmail;
+    } else {
+        let userEmail = ""
+        return userEmail;
+    }
+    
+  };
+  
+  username = Username()
+  userEmail = UserEmail()
 
-exports.coursepage = async(req, res) => {
-    let idd = req.params.id;
-    let result = "";
-    result = await courses.findOne({ _id: idd });
-   
-    let pagename ="coursepage"
-    console.log(authenticated)
-    res.render("course", { result, authenticated, pagename })
-}
+  res.render("about", { pagename, authenticated, username, userEmail });
+};
 
-exports.test = async(req, res, next) => {
-    let doc = ""
-    docx = await test.find({})
-    let pagename = "test";
+exports.courses = async (req, res, next) => {
+  let doc = "";
+  doc = await courses.find({});
+  let pagename = "courses";
+  const authenticated = req.isAuthenticated();
+  
+  var Username = function() {
+    if (authenticated) {
+      let username = req.user.name;
+      return username;
+    } else {
+        let username = ""
+        return username;
+    }
+    
+  };
 
-    res.render("tests", { doc, pagename })
+  var UserEmail = function() {
+    if (authenticated) {
+      let userEmail = req.user.email;
+      return userEmail;
+    } else {
+        let userEmail = ""
+        return userEmail;
+    }
+    
+  };
+  
+  username = Username()
+  userEmail = UserEmail()
 
-}
+  res.render("courses", { doc, pagename, authenticated, userEmail, username });
+};
+
+exports.coursepage = async (req, res) => {
+  let idd = req.params.id;
+  let result = "";
+  result = await courses.findOne({ _id: idd });
+  let pagename = "coursepage";
+  
+  var Username = function() {
+    if (authenticated) {
+      let username = req.user.name;
+      return username;
+    } else {
+        let username = ""
+        return username;
+    }
+    
+  };
+
+  var UserEmail = function() {
+    if (authenticated) {
+      let userEmail = req.user.email;
+      return userEmail;
+    } else {
+        let userEmail = ""
+        return userEmail;
+    }
+    
+  };
+  
+  username = Username()
+  userEmail = UserEmail()
+
+  res.render("course", { result, authenticated, pagename, username, userEmail });
+};
+
+exports.test = async (req, res, next) => {
+  let doc = "";
+  docx = await test.find({});
+  let pagename = "test";
+  const authenticated = req.isAuthenticated();
+
+  var Username = function() {
+    if (authenticated) {
+      let username = req.user.name;
+      return username;
+    } else {
+        let username = ""
+        return username;
+    }
+    
+  };
+
+  var UserEmail = function() {
+    if (authenticated) {
+      let userEmail = req.user.email;
+      return userEmail;
+    } else {
+        let userEmail = ""
+        return userEmail;
+    }
+    
+  };
+  
+  username = Username()
+  userEmail = UserEmail()
+
+  res.render("tests", { doc, authenticated, pagename, username, userEmail });
+};
 
 // exports.coursepage = async(req, res) => {
 //     let idd = req.params.id;
@@ -69,50 +191,166 @@ exports.test = async(req, res, next) => {
 //     res.render("course", { result })
 // }
 
-
-
-
 exports.classroom = (req, res) => {
-    let pagename = "classroom";
-    
-    const authenticated = req.isAuthenticated();
+  let pagename = "classroom";
 
-    res.render("classroom", {pagename, authenticated,})
-}
+  const authenticated = req.isAuthenticated();
+
+  var Username = function() {
+    if (authenticated) {
+      let username = req.user.name;
+      return username;
+    } else {
+        let username = ""
+        return username;
+    }
+    
+  };
+
+  var UserEmail = function() {
+    if (authenticated) {
+      let userEmail = req.user.email;
+      return userEmail;
+    } else {
+        let userEmail = ""
+        return userEmail;
+    }
+    
+  };
+  
+  username = Username()
+  userEmail = UserEmail()
+
+  res.render("classroom", { pagename, authenticated, username, userEmail });
+};
 
 exports.topic = (req, res) => {
-    let pagename = "topic";
-    const authenticated = req.isAuthenticated();
+  let pagename = "topic";
+  const authenticated = req.isAuthenticated();
    
-    res.render("CMS/topic", {pagename, authenticated})
-}
+  var Username = function() {
+    if (authenticated) {
+      let username = req.user.name;
+      return username;
+    } else {
+        let username = ""
+        return username;
+    }
+    
+  };
+
+  var UserEmail = function() {
+    if (authenticated) {
+      let userEmail = req.user.email;
+      return userEmail;
+    } else {
+        let userEmail = ""
+        return userEmail;
+    }
+    
+  };
+  
+  username = Username()
+  userEmail = UserEmail()
+
+  res.render("CMS/topic", { pagename, authenticated, username, userEmail });
+};
 
 exports.contact = (req, res) => {
-    let pagename = "contact";
-    const authenticated = req.isAuthenticated();
+  let pagename = "contact";
+  const authenticated = req.isAuthenticated();
 
-    res.render("contact", {pagename, user: req.user , authenticated})
-}
+  var Username = function() {
+    if (authenticated) {
+      let username = req.user.name;
+      return username;
+    } else {
+        let username = ""
+        return username;
+    }
+    
+  };
 
+  var UserEmail = function() {
+    if (authenticated) {
+      let userEmail = req.user.email;
+      return userEmail;
+    } else {
+        let userEmail = ""
+        return userEmail;
+    }
+    
+  };
+  
+  username = Username()
+  userEmail = UserEmail()
+
+  res.render("contact", { pagename, user: req.user, authenticated, username, userEmail });
+};
 
 exports.summary = (req, res) => {
-
-    let pagename = "summary";
-    const authenticated = req.isAuthenticated();
+  let pagename = "summary";
+  const authenticated = req.isAuthenticated();
+  
+  var Username = function() {
+    if (authenticated) {
+      let username = req.user.name;
+      return username;
+    } else {
+        let username = ""
+        return username;
+    }
     
-    res.render("summary", {pagename, authenticated})
-}
+  };
+
+  var UserEmail = function() {
+    if (authenticated) {
+      let userEmail = req.user.email;
+      return userEmail;
+    } else {
+        let userEmail = ""
+        return userEmail;
+    }
+    
+  };
+  
+  username = Username()
+  userEmail = UserEmail()
+
+  res.render("summary", { pagename, authenticated, username, userEmail });
+};
 
 exports.coursepage = (req, res) => {
-    let pagename = "coursepage";
-    const authenticated = req.isAuthenticated();
+  let pagename = "coursepage";
+  const authenticated = req.isAuthenticated();
    
-    res.render("course", {pagename, authenticated})
-}
+  var Username = function() {
+    if (authenticated) {
+      let username = req.user.name;
+      return username;
+    } else {
+        let username = ""
+        return username;
+    }
+    
+  };
 
+  var UserEmail = function() {
+    if (authenticated) {
+      let userEmail = req.user.email;
+      return userEmail;
+    } else {
+        let userEmail = ""
+        return userEmail;
+    }
+    
+  };
+  
+  username = Username()
+  userEmail = UserEmail()
 
-
-
+  res.render("course", { pagename, authenticated, username, userEmail });
+};
 
 // exports.renderPage = function(req, res) {
 //     let navIndex = req.path.substr(1);
